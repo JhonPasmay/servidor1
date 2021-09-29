8f02f049f1ef1d508f8a34a09d4b37c6b23d11c9
JhonPasmay <89862598+JhonPasmay@users.noreply.github.com>
2021-09-28 21:30:05 -0500

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,23 @@
 //https://www.eclipse.org/paho/clients/js/
 
+var btn=document.getElementById('btn'), contador=0;
+function cambio()
+{ if (contador==0)
+  {
+  message = new Paho.MQTT.Message("ENCENDER");
+  message.destinationName = "pasmayj@gmail.com/t2";
+  client.send(message);
+  contador=1;
+  }
+ else
+  {
+  message = new Paho.MQTT.Message("APAGAR");
+  message.destinationName = "pasmayj@gmail.com/t2";
+  client.send(message);
+  contador=0;
+  }
+}
+
 function LED1_On() {
   //alert("led on");
   console.log("led on");
@@ -18,23 +36,22 @@ function LED1_Off(){
   //document.getElementById("sensor").innerHTML="led off";
 }
 
-
-function HISTORIAL_S1(){
-     console.log("led on");
+//funcion para recibir datos:
+//function HISTORIAL_S1(){
+  //   console.log("led on");
   //document.getElementById("sensor").innerHTML=client.historial;
-  message = new Paho.MQTT.Message("ON");
-    message.destinationName = "pasmayj@gmail.com/t1";
-      client.send(message);
-}
+  //message = new Paho.MQTT.Message("ON");
+    //message.destinationName = "pasmayj@gmail.com/t1";
+     // client.send(message);
+//}
 
-function HISTORIAL_S2(){
-     console.log("led on");
+//function HISTORIAL_S2(){
+  //   console.log("led on");
   //document.getElementById("sensor").innerHTML=client.historial;
-  message = new Paho.MQTT.Message("ON");
-    message.destinationName = "pasmayj@gmail.com/t1";
-      client.send(message);
-}
-
+ // message = new Paho.MQTT.Message("ON");
+   // message.destinationName = "pasmayj@gmail.com/t1";
+     // client.send(message);
+//}
 
 
 
@@ -88,4 +105,19 @@ function HISTORIAL_S2(){
     var sms = message.payloadString.split(";")
     document.getElementById("sensor").innerHTML=sms[0];
     document.getElementById("sensor1").innerHTML=sms[1];
+    //comando para poner el sensor desde esp32
+    document.getElementById("sensor").innerHTML=message.payloadString;
+    if(message.payloadString==='ENCENDER'){
+     document.getElementById("imagen").src="https://previews.123rf.com/images/sarahdesign/sarahdesign1410/sarahdesign141002939/32951945-ilustraci%C3%B3n-del-bot%C3%B3n-de-encendido.jpg";
+    } else if(message.payloadString==='APAGAR'){
+      document.getElementById('imagen').src= "https://www.aboutespanol.com/thmb/mg6tbcWB-pPNw5v2M9Sc8RSd8F0=/735x0/PC-No-Se-Apaga-565ff7e13df78cedb09eea54.png";
+      
+    }
+    if(message.payloadString==='ENCENDER'){
+      document.getElementById("btn").innerHTML="Apagar";
+    } else if(message.payloadString==='APAGAR'){
+    document.getElementById("btn").innerHTML="Encender";
+    }
+
+
   }
